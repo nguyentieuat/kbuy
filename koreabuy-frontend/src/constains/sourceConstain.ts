a23d41fdcebde41dbff6a0ea644dd0d225f78201
nguyentieuat <thanhluan.kma@gmail.com>
2026-06-05 22:14:57 +0700

    update coupou and ship min internal

--- koreabuy-frontend/src/constains/sourceConstain.ts
+++ koreabuy-frontend/src/constains/sourceConstain.ts
@@ -1,8 +1,10 @@
 // constains/sourceConstain.ts
 
 export const SOURCES = [
+  { label: "CheongKwanJang", value: "kgc" },
   { label: "Olive Young", value: "oliveyoung" },
   { label: "T1 shop", value: "t1" },
   { label: "GenG shop", value: "geng" },
   { label: "Musinsa", value: "musinsa" },
+  
 ];
